6237cbfb815e463dcb32df9d8fbfdf333a76129f
Prince Jules Correa <jrincepules@gmail.com>
2024-02-01 13:41:05 +0800

    logout fix #2

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,12 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
     const response = NextResponse.json({
       message: "Logout successful",
       success: true,
     });
-    response.cookies.delete("token");
+    const token = request.cookies.get("token");
+    if (token) {
+      response.cookies.delete("token"); // Delete the 'token' cookie
+    } else {
+      console.error("No 'token' cookie found."); // Handle the case where 'token' cookie is not found
+    }
 
     return response;
   } catch (error: any) {
